b70dfdca3ecc4f20a7e16b106c36c93dbbe6ff31
Kristian Stafford Smith <kristianss7890@gmail.com>
2019-07-07 00:25:34 +0100

    refactored partone.py: - the model for the user is now being defined in the define_entity function, taking the database as an argument. - the database is also now being setup in a separate function called define_database. - create_entity renamed to create_entities and is now taking a database as an argument

--- partone.py
+++ partone.py
@@ -1,20 +1,25 @@
 from pony.orm import Database, db_session, Required
 
-db = Database('postgres', 'postgres://localhost:5432/user-database')
+# db = Database('postgres', 'postgres://localhost:5432/user-database')
 
-class User(db.Entity):
-    username = Required(str)
-    email = Required(str)
-    password = Required(str)
-    age = Required(int)
-    location = Required(str)
+def define_entity(db):
+    class User(db.Entity):
+        username = Required(str)
+        email = Required(str)
+        password = Required(str)
+        age = Required(int)
+        location = Required(str)
 
-db.generate_mapping(create_tables=True)
+def define_database(**db_params):
+    db = Database(**db_params)
+    define_entity(db)
+    db.generate_mapping(create_tables=True)
+    return db
 
 @db_session
-def create_entity(database):
+def create_entities(database):
     database.User(username="Josh01", email="josh@gmail.com", password="pass", age=25, location='London')
     database.User(username="Sarah02", email="sarah@gmail.com", password="pass", age=34, location='New York')
     database.User(username="Harris03", email="harris@gmail.com", password="pass", age=45, location='Toronto')
 
-create_entity(db)
+# create_entity(db)
--- parttwo.py
+++ parttwo.py
@@ -1,17 +1,1 @@
-import unittest
-import testing.postgresql
-from partone import create_entity
 
-# store data that currently exists in the real database for comparison purposes in test
-prev_data = testing.postgresql.Postgresql(copy_data_from='postgres://localhost:5432/user-database')
-
-class TestCase(unittest.TestCase):
-    def setUp(self):
-        self.postgresql = testing.postgresql.Postgresql()
-
-    def test_create_entity(self):
-        # I need to re-write create_entity func to take the database of interest as an argument
-        create_entity()
-
-    def tearDown(self):
-        self.postgresql.stop()
