722628be1e388d0b5735ba9c68ca267f4fc8e37d
Kristian Stafford Smith <kristianss7890@gmail.com>
2019-07-06 17:38:00 +0100

    generated mapping on the database object, defined and invoked the function to store a set of user data in the database, part one complete

--- partone.py
+++ partone.py
@@ -1,4 +1,4 @@
-from pony.orm import Database, Required
+from pony.orm import Database, db_session, Required
 
 db = Database('postgres', 'postgres://localhost:5432/user-database')
 
@@ -8,3 +8,13 @@ class User(db.Entity):
     password = Required(str)
     age = Required(int)
     location = Required(str)
+
+db.generate_mapping(create_tables=True)
+
+@db_session
+def create_entity():
+    User(username="Josh01", email="josh@gmail.com", password="pass", age=25, location='London')
+    User(username="Sarah02", email="sarah@gmail.com", password="pass", age=34, location='New York')
+    User(username="Harris03", email="harris@gmail.com", password="pass", age=45, location='Toronto')
+
+create_entity()
