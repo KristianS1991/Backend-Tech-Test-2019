ab9c10a17c3aa2b3d9c5fd3933ab026364b5ae2d
Kristian Stafford Smith <kristianss7890@gmail.com>
2019-07-07 00:04:08 +0100

    create_entity function needs to take database parameters as an argument in order to test against a temporary database. After some research, I have decided to use flask-testing rather than the postgresql.testing module. This is because flask-testing appears to be more fit for purpose with PonyORM

--- partone.py
+++ partone.py
@@ -12,9 +12,9 @@ class User(db.Entity):
 db.generate_mapping(create_tables=True)
 
 @db_session
-def create_entity():
-    User(username="Josh01", email="josh@gmail.com", password="pass", age=25, location='London')
-    User(username="Sarah02", email="sarah@gmail.com", password="pass", age=34, location='New York')
-    User(username="Harris03", email="harris@gmail.com", password="pass", age=45, location='Toronto')
+def create_entity(database):
+    database.User(username="Josh01", email="josh@gmail.com", password="pass", age=25, location='London')
+    database.User(username="Sarah02", email="sarah@gmail.com", password="pass", age=34, location='New York')
+    database.User(username="Harris03", email="harris@gmail.com", password="pass", age=45, location='Toronto')
 
-create_entity()
+create_entity(db)
--- parttwo.py
+++ parttwo.py
@@ -1,10 +1,17 @@
 import unittest
-import testing.postrgresql
-# from partone import create_entity
+import testing.postgresql
+from partone import create_entity
+
+# store data that currently exists in the real database for comparison purposes in test
+prev_data = testing.postgresql.Postgresql(copy_data_from='postgres://localhost:5432/user-database')
 
 class TestCase(unittest.TestCase):
     def setUp(self):
-        self.postrgresql = testing.postgresql.Postgresql()
+        self.postgresql = testing.postgresql.Postgresql()
+
+    def test_create_entity(self):
+        # I need to re-write create_entity func to take the database of interest as an argument
+        create_entity()
 
     def tearDown(self):
-        self.postrgresql.stop()
+        self.postgresql.stop()
