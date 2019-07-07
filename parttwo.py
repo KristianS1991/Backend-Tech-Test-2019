3a0385817a9e296cd12324d726d5ffe9f33e8cc0
Kristian Stafford Smith <kristianss7890@gmail.com>
2019-07-07 02:09:21 +0100

    testing setUp function defined in TestApp class for parttwo.py

--- parttwo.py
+++ parttwo.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_testing import TestCase
+from partone import define_database
+from partone import create_entities
 
 class TestApp(TestCase):
+    PONY_DB_PARAMS = dict(provider='sqlite', filename='/tmp/test.db')
+
     def create_app(self):
         app = Flask(__name__)
         app.config['TESTING'] = True
         return app
 
-    
+    def setUp(self):
+        self.db = define_database(**self.PONY_DB_PARAMS)
+        create_entities(self.db)
